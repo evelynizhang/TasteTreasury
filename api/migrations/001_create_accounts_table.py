3ae8e08e67026b0088704297085749082d3013cd
Ni <evelyn.z.mccoy@gmail.com>
2023-08-16 10:41:07 -0700

    fixed migrations

--- api/migrations/001_create_accounts_table.py
+++ api/migrations/001_create_accounts_table.py
@@ -2,7 +2,7 @@ steps = [
     [
         """
 	CREATE TABLE accounts (
-		id SERIAL PRIMARY KEY NOT NULL,
+		id SERIAL PRIMARY KEY NOT NULL UNIQUE,
 		username VARCHAR(50) UNIQUE NOT NULL,
 		email VARCHAR(254) UNIQUE NOT NULL,
 		hashed_password VARCHAR(1000) NOT NULL
