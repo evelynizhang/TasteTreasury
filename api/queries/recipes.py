c7d68225513847b05eea5393b34d09713f2df8e8
jonah heskje <jonah.heskje@gmail.com>
2023-08-22 15:27:33 -0500

    added get_one_recipe to routers.recipes.py and get_one method to queries.recipes.RecipeQueries

--- api/queries/recipes.py
+++ api/queries/recipes.py
@@ -1,4 +1,4 @@
-from models import RecipeIn, RecipeOut
+from models import RecipeIn, RecipeOut, Direction, HttpError
 from queries.pool import pool
 
 
@@ -8,7 +8,63 @@ class RecipeQueries:
             # get a cursor to run SQL
             with conn.cursor() as cur:
                 # run our INSERT statement
-                cur.execute()
+                recipe_data = cur.execute(
+                    """
+                    SELECT name, prep_time, servings, picture_url
+                    FROM recipes
+                    WHERE id = %s
+                    """,
+                    [recipe_id],
+                )
+                recipe = recipe_data.fetchone()  # tuple containing specified values from recipe table
+                if recipe == None:
+                    return HttpError(detail="Unable to match id to existing recipe")
+                ingredients_data = cur.execute(
+                    """
+                    SELECT item
+                    FROM ingredients
+                    WHERE recipe_id = %s
+                    """,
+                    [recipe_id],
+                )
+                # convert from list of tuples to list of tuple[0] elements
+                ingredients = []
+                for ingredient in ingredients_data.fetchall():
+                    ingredients.append(ingredient[0])
+                directions_data = cur.execute(
+                    """
+                    SELECT recipe_step, step_number
+                    FROM directions
+                    WHERE recipe_id = %s
+                    """,
+                    [recipe_id],
+                )
+                # convert from list of tuples to list of Direction objects
+                directions = []
+                for direction in directions_data.fetchall():
+                    directions.append(Direction(step_number=direction[1], recipe_step=direction[0]))
+                tags_data = cur.execute(
+                    """
+                    SELECT tag_name
+                    FROM recipe_tags
+                    WHERE recipe_id = %s
+                    """,
+                    [recipe_id],
+                )
+                # convert from list of tuples to list of tuple[0] elements
+                tags = []
+                for tag in tags_data.fetchall():
+                    tags.append(tag[0])
+                return RecipeOut(
+                    id=recipe_id,
+                    name=recipe[0],
+                    prep_time=recipe[1],
+                    servings=recipe[2],
+                    picture_url=recipe[3],
+                    ingredients=ingredients,
+                    directions=directions,
+                    tags=tags,
+                )
 
     def create(self, info: RecipeIn):
         with pool.connection() as conn:
