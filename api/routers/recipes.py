180587f363711afd333ff85c02522765f7ad6109
jonah heskje <jonah.heskje@gmail.com>
2023-08-24 11:20:04 -0500

    protected create, put, and delete routes for recipes

--- api/routers/recipes.py
+++ api/routers/recipes.py
@@ -1,26 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models import (
-    RecipeOut,
-    RecipeIn,
-    HttpError,
-    RecipeCardOut,
-    DeleteResponse,
-)
+from models import RecipeOut, RecipeIn, HttpError, RecipeCardOut, DeleteResponse, AccountOut
 from queries.recipes import RecipeQueries
 from typing import List
+from authenticator import authenticator
 
 
 router = APIRouter()
 
 
 @router.post("/api/recipes/mine", response_model=RecipeOut | HttpError)
-def create_recipe(info: RecipeIn, queries: RecipeQueries = Depends()):
+def create_recipe(
+    info: RecipeIn,
+    queries: RecipeQueries = Depends(),
+    account_data: AccountOut = Depends(authenticator.get_current_account_data),
+):
     try:
         recipe = queries.create(info)
     except:
-        raise HTTPException(
-            status_code=400, detail="Unable to create a recipe"
-        )
+        raise HTTPException(status_code=400, detail="Unable to create a recipe")
     return recipe
 
 
@@ -42,22 +39,25 @@ def get_one_recipe(recipe_id: int, queries: RecipeQueries = Depends()):
     return recipe
 
 
-@router.delete(
-    "/api/recipes/{recipe_id}", response_model=DeleteResponse | HttpError
-)
-def delete_one_recipe(recipe_id: int, queries: RecipeQueries = Depends()):
+@router.delete("/api/recipes/{recipe_id}", response_model=DeleteResponse | HttpError)
+def delete_one_recipe(
+    recipe_id: int,
+    queries: RecipeQueries = Depends(),
+    account_data: AccountOut = Depends(authenticator.get_current_account_data),
+):
     try:
         is_deleted = queries.delete(recipe_id)
     except:
-        raise HTTPException(
-            status_code=400, detail="Unable to delete the recipe"
-        )
+        raise HTTPException(status_code=400, detail="Unable to delete the recipe")
     return is_deleted
 
 
 @router.put("/api/recipes/{recipe_id}", response_model=RecipeOut | HttpError)
 def update_recipe(
-    recipe_id: int, info: RecipeIn, queries: RecipeQueries = Depends()
+    recipe_id: int,
+    info: RecipeIn,
+    queries: RecipeQueries = Depends(),
+    account_data: AccountOut = Depends(authenticator.get_current_account_data),
 ):
     try:
         recipe = queries.update(recipe_id, info)
