fb35edf0cf489430df6ba41f5036cbd42218ef72
Ni <evelyn.z.mccoy@gmail.com>
2023-08-29 08:50:21 -0700

    completed unit tests for get all recipes and get one recipe

---
+++ api/tests/test_recipes.py
@@ -0,0 +1,70 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.recipes import RecipeQueries
+
+
+client = TestClient(app)
+
+
+class FakeRecipeQueries:
+    def get_all(self):
+        return [
+            {
+                "id": 3,
+                "name": "string",
+                "prep_time": "string",
+                "servings": 0,
+                "picture_url": "https://pinchofyum.com/wp-content/uploads/Instant-Pot-Mac-and-Cheese-Square.jpg",
+                "account_id": 3,
+            }
+        ]
+
+    def get_one(self, recipe_id: int):
+        return {
+            "id": recipe_id,
+            "name": "string",
+            "prep_time": "string",
+            "servings": 0,
+            "picture_url": "https://pinchofyum.com/wp-content/uploads/Instant-Pot-Mac-and-Cheese-Square.jpg",
+            "account_id": 3,
+            "ingredients": ["string"],
+            "directions": [{"step_number": 0, "recipe_step": "string"}],
+            "tags": ["chicken"],
+        }
+
+
+def test_get_all_recipes():
+    app.dependency_overrides[RecipeQueries] = FakeRecipeQueries
+    res = client.get("/api/recipes")
+    data = res.json()
+
+    assert res.status_code == 200
+    assert data == [
+        {
+            "id": 3,
+            "name": "string",
+            "prep_time": "string",
+            "servings": 0,
+            "picture_url": "https://pinchofyum.com/wp-content/uploads/Instant-Pot-Mac-and-Cheese-Square.jpg",
+            "account_id": 3,
+        }
+    ]
+
+
+def test_get_one_recipe():
+    app.dependency_overrides[RecipeQueries] = FakeRecipeQueries
+    res = client.get("/api/recipes/3")
+    data = res.json()
+
+    assert res.status_code == 200
+    assert data == {
+        "id": 3,
+        "name": "string",
+        "prep_time": "string",
+        "servings": 0,
+        "picture_url": "https://pinchofyum.com/wp-content/uploads/Instant-Pot-Mac-and-Cheese-Square.jpg",
+        "account_id": 3,
+        "ingredients": ["string"],
+        "directions": [{"step_number": 0, "recipe_step": "string"}],
+        "tags": ["chicken"],
+    }
