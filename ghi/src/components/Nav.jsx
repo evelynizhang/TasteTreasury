5a1942333dc522b30277bd366f18f3423d7e32ed
erick <erickmconners@gmail.com>
2023-09-08 18:33:17 -0400

    details

--- ghi/src/components/Nav.jsx
+++ ghi/src/components/Nav.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Nav() {
+  const logo = process.env.PUBLIC_URL + "/favicon.ico";
   const { data: account } = useGetTokenQuery();
   const [logout, logoutResponse] = useLogoutMutation();
   const navigate = useNavigate();
@@ -13,9 +14,9 @@ function Nav() {
   }, [logoutResponse]);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand ml-5" to="/">
-        Home
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mx-3">
+      <NavLink className="navbar-brand" to="/">
+        <img className="icon" src={logo} />
       </NavLink>
       <button
         className="navbar-toggler"
@@ -28,10 +29,7 @@ function Nav() {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div
-        className="collapse navbar-collapse mr-5 "
-        id="navbarSupportedContent"
-      >
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <NavLink className="nav-link" to="/recipes">
@@ -54,10 +52,7 @@ function Nav() {
           )}
         </ul>
         {!account && (
-          <button
-            className="btn btn-outline-primary ml-2"
-            onClick={() => navigate("/login")}
-          >
+          <button className="btn ml-2" onClick={() => navigate("/login")}>
             Account
           </button>
         )}
